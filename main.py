6b7a660a7ca8463e8e0502e015f2ece28fd92ac8
sadamccr <sadamccr@gmail.com>
2023-01-11 13:43:58 +0500

    done

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 
+with open('medicine.json') as f:
+    data = json.load(f)
+    root = data['medicines']
+    add = {"name": "paracatamol", "price": 25, "id": 3}
+    root.append(add)
 
-def write_json(data, filename="medicine.json"):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=2)
 
+def write_json(mydata):
+    with open('medicine.json', 'w') as ff:
+        json.dump(mydata, ff)
 
-with open('medicine.json') as ff:
-    data = json.load(ff)
-    temp = data['medicines']
-    y = {"name": "panadol", "price": 25, "id": 3}
-    temp.append(y)
 
 write_json(data)
